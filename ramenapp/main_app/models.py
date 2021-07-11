bb530e78dc8c5916f244b192e72ae2aab440a23a
Nicole Stawikowski <nicole.stawikowski@gmail.com>
2021-07-11 19:38:29 -0400

    implement user auth

--- ramenapp/main_app/models.py
+++ ramenapp/main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Ramen(models.Model):
+    brand = models.CharField(max_length=100)
+    flavour = models.CharField(max_length=100)
+    spice = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.CharField(max_length=100)    
+
+    
--- ramenapp/main_app/views.py
+++ ramenapp/main_app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+# Define the home view
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'registration/login.html')
+
+def userprofile(request):
+    return render(request, 'userprofile.html')    
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
