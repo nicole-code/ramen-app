e84ccf5eaa9fbcba08300d23274fca71e02f2805
zarialea <81105892+zarialea@users.noreply.github.com>
2021-07-13 12:30:20 -0400

    update

--- main_app/models.py
+++ main_app/models.py
@@ -1,19 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
 class Ramen(models.Model):
     brand = models.CharField(max_length=100)
     flavour = models.CharField(max_length=100)
     spice = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ingredient = models.ManyToManyField(Ingredient)
 
-class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    profile_pic = models.ImageField(null=True, blank=True)
-    email = models.CharField(max_length=100)    
 
     
