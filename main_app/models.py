0b4c69697957ad8c9f58b3116eee7e4e40aee7b6
k-pura <85129355+k-pura@users.noreply.github.com>
2021-07-13 11:20:13 -0400

    model fix

--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
 class Ramen(models.Model):
     brand = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     Ingredient = models.ManyToManyField(Ingredient)
-    def __str__(self):
-        return self.name
-
-class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
     def __str__(self):
         return self.name
 
