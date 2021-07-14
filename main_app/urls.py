015e639e2a732a38e79475a1e0011bda44123af4
k-pura <85129355+k-pura@users.noreply.github.com>
2021-07-14 01:33:50 -0400

    Create, Ramen Detail, Delete, community home, community detail, add ingredients to model

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,11 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('community/<int:ramen_id>/', views.communitydetail, name= 'communitydetail'),
     path('login/', views.login_user, name='login'),
     path('userprofile/', views.user_profile, name='user'),
     path('buildramen/new', views.buildramen_new, name='buildramen_new'),
     path('accounts/signup/', views.signup, name='signup'),
-    path('buildramen/post', views.buildramen_create, name='buildramen_create' )
+    path('post/', views.buildramen_create, name='buildramen_create' ),
+    path('buildramen/<int:ramen_id>/', views.ramendetail, name= 'ramendetail'),
+    path('buildramen/<int:ramen_id>/delete/', views.ramen_delete, name='ramendelete'),
     # path('usereditprofile/', views.edit_profile, name='edit_profile'),
     # path('updateuser/<int:profile_id>/', views.user_update_form),
 
